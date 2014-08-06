b6238272a5b8ae5e6452f9b056d82a314f8bd022
alexeyfinkel <finkel@physics.umn.edu>
2014-08-06 12:25:06 -0500

    still fixing conflicts in photonBucket

--- photonBucket/CalibratedHighEtaPhotons/python/calibratedhighetaphotons_cfi.py
+++
@@ -1,6 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-calibratedHighEtaPhotons = cms.EDProducer('CalibratedHighEtaPhotons',
-    photons       = cms.InputTag( "photons" ),
-	ecalRecHits   = cms.InputTag("reducedEcalRecHitsEE"),
-)
